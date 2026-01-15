075060241cc97249d18c09ecf5d321119c03e0be
Shubhanshu <shubhanshus450@gmail.com>
2026-01-16 01:32:26 +0530

    feat: implement anonymous authentication, JWT verification middleware and jwt token service

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,18 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+
+export const generateAccessAndRefreshTokens = async (userId) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+
+  user.refreshToken = refreshToken;
+  await user.save({ validateBeforeSave: false });
+
+  return { accessToken, refreshToken };
+};
